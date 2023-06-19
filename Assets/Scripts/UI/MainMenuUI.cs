fd5f74ebaea00c8d4885ec782eba62f1441eeb24
Navat2007 <navat2007@yandex.ru>
2023-06-19 16:00:17 +0300

    add

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -17,23 +17,32 @@ public class MainMenuUI : MonoBehaviour
             EventBus.GameEvents.OnGameStarted?.Invoke();
         });
         
-        _optionsButton.onClick.AddListener(() =>
+        if(_optionsButton != null)
         {
-            EventBus.UIEvents.OnSettingsWindowShow?.Invoke();
-        });
-        
-        _leaderButton.onClick.AddListener(() =>
+            _optionsButton.onClick.AddListener(() =>
+            {
+                EventBus.UIEvents.OnSettingsWindowShow?.Invoke();
+            });
+        }
+
+        if (_leaderButton != null)
         {
-            EventBus.UIEvents.OnLeaderBoardWindowShow?.Invoke();
-        });
-        
-        _exitButton.onClick.AddListener(() =>
+            _leaderButton.onClick.AddListener(() =>
+            {
+                EventBus.UIEvents.OnLeaderBoardWindowShow?.Invoke();
+            });
+        }
+
+        if (_exitButton != null)
         {
+            _exitButton.onClick.AddListener(() =>
+            {
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
 #endif
-            Application.Quit();
-        });
+                Application.Quit();
+            });
+        }
 
         EventBus.GameEvents.OnGameStarted += OnStartLevel;
         EventBus.UIEvents.OnMainMenuWindowShow += OnMainMenuWindowShow;
